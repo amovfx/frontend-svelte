9fb026bc8959fec759d9bafa1943e22be7c5a9fe
amovfx <45839100+amovfx@users.noreply.github.com>
2022-06-29 11:44:09 -0600

    added Material UI theme and components

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,9 @@
+<script>
+    import Button from '@smui/button';
+  </script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h2>test</h2>
+<Button>My Material Button</Button>
+<Button>Poop</Button>
